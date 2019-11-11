f950bee975b3ee5ee523abc1c7fb175576804adc
Makon Cline <makoncline@gmail.com>
2019-11-10 18:58:12 -0600

    make max width xs

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import InputAdornment from '@material-ui/core/InputAdornment';
 import Slider from '@material-ui/core/Slider';
 import NumberFormat from 'react-number-format';
 import PropTypes from 'prop-types';
-import Box from '@material-ui/core/Box';
 import { ThemeProvider } from '@material-ui/styles';
 import { createMuiTheme } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -199,147 +198,147 @@ function App() {
 
   return (
     <React.Fragment>
-    <CssBaseline />
-    <Box maxWidth='xs' className={classes.root}>
-    <Container maxWidth='80%'>
-    <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-        <Grid container justify='space-between' >
-          <Grid item xs={3} >
-            <Typography variant='h4' component='h1'>Tip</Typography>
-          </Grid>
-          <Grid item xs={3} >
-            <Button aria-label="Dark mode" fullWidth onClick={() => handleClick('dark')}><Brightness4Icon/></Button>
-          </Grid>
-          <Grid item xs={3} >
-            <Button aria-label="Reset" fullWidth onClick={() => handleClick('reset')}>Reset</Button>
-          </Grid>
-          <Grid item xs={3}>
-            <Button aria-label="Clear" fullWidth onClick={() => handleClick('clear')}>Clear</Button>
-          </Grid>
-        </Grid>
-        <TextField
-          id="billAmount"
-          label='Bill Amount'
-          margin="normal"
-          variant="outlined"
-          fullWidth
-          value={state.billAmount > 0 ? state.billAmount : ''}
-          onChange={handleChange('billAmount')}
-          InputProps={
-            state.billAmount > 0 ? {
-              startAdornment: <InputAdornment position="start">$</InputAdornment>,
-              inputComponent: NumberFormatCurrency,
-            } : { inputComponent: NumberFormatCurrency, }
-          }
-        />
-        <TextField
-          id="subtotal"
-          label='Subtotal'
-          margin="normal"
-          variant="outlined"
-          fullWidth
-          value={subtotal > 0 ? subtotal : ''}
-          onChange={handleChange('subtotal')}
-          InputProps={
-            state.subtotal > 0 ? {
-              startAdornment: <InputAdornment position="start">$</InputAdornment>,
-              inputComponent: NumberFormatCurrency,
-            } : { inputComponent: NumberFormatCurrency, }
-          }
-        />
-        <TextField
-          id="tax"
-          label={`Tax (${(state.taxPercent * 100).toFixed(2)}%)`}
-          margin="normal"
-          variant="outlined"
-          fullWidth
-          value={tax > 0 ? tax : ''}
-          onChange={handleChange('tax')}
-          InputProps={
-            state.tax > 0 ? {
-              startAdornment: <InputAdornment position="start">$</InputAdornment>,
-              inputComponent: NumberFormatCurrency,
-            } :
-              {
-                inputComponent: NumberFormatCurrency,
+      <Container maxWidth='xs' className={classes.root}>
+        <Container maxWidth='80%'>
+          <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <Grid container justify='space-between' >
+              <Grid item xs={3} >
+                <Typography variant='h4' component='h1'>Tip</Typography>
+              </Grid>
+              <Grid item xs={3} >
+                <Button aria-label="Dark mode" fullWidth onClick={() => handleClick('dark')}><Brightness4Icon /></Button>
+              </Grid>
+              <Grid item xs={3} >
+                <Button aria-label="Reset" fullWidth onClick={() => handleClick('reset')}>Reset</Button>
+              </Grid>
+              <Grid item xs={3}>
+                <Button aria-label="Clear" fullWidth onClick={() => handleClick('clear')}>Clear</Button>
+              </Grid>
+            </Grid>
+            <TextField
+              id="billAmount"
+              label='Bill Amount'
+              margin="normal"
+              variant="outlined"
+              fullWidth
+              value={state.billAmount > 0 ? state.billAmount : ''}
+              onChange={handleChange('billAmount')}
+              InputProps={
+                state.billAmount > 0 ? {
+                  startAdornment: <InputAdornment position="start">$</InputAdornment>,
+                  inputComponent: NumberFormatCurrency,
+                } : { inputComponent: NumberFormatCurrency, }
               }
-          }
-        />
-        <Slider
-          id='taxSlider'
-          valueLabelDisplay="auto"
-          valueLabelFormat={() => parseInt(state.taxPercent * 100) + '%'}
-          step={1}
-          marks
-          min={0}
-          max={30}
-          value={parseInt(state.taxPercent * 100)}
-          onChange={handleChange('taxPercent')}
-        />
-        <TextField
-          id="tip"
-          label={`Tip (${(state.tipPercent * 100).toFixed(2)}%)`}
-          margin="normal"
-          variant="outlined"
-          fullWidth
-          value={tip > 0 ? tip : ''}
-          onChange={handleChange('tip')}
-          InputProps={
-            state.tip > 0 ? {
-              startAdornment: <InputAdornment position="start">$</InputAdornment>,
-              inputComponent: NumberFormatCurrency,
-            } :
-              {
-                inputComponent: NumberFormatCurrency,
+            />
+            <TextField
+              id="subtotal"
+              label='Subtotal'
+              margin="normal"
+              variant="outlined"
+              fullWidth
+              value={subtotal > 0 ? subtotal : ''}
+              onChange={handleChange('subtotal')}
+              InputProps={
+                state.subtotal > 0 ? {
+                  startAdornment: <InputAdornment position="start">$</InputAdornment>,
+                  inputComponent: NumberFormatCurrency,
+                } : { inputComponent: NumberFormatCurrency, }
               }
-          }
-        />
-        <Slider
-          id='tipSlider'
-          valueLabelDisplay="auto"
-          valueLabelFormat={() => parseInt(state.tipPercent * 100) + '%'}
-          step={1}
-          marks
-          min={0}
-          max={30}
-          value={parseInt(state.tipPercent * 100)}
-          onChange={handleChange('tipPercent')}
-        />
-        <TextField
-          id="total"
-          label='Total'
-          margin="normal"
-          variant="outlined"
-          fullWidth
-          value={total > 0 ? total : ''}
-          onChange={handleChange('total')}
-          InputProps={
-            state.total > 0 ? {
-              startAdornment: <InputAdornment position="start">$</InputAdornment>,
-              inputComponent: NumberFormatCurrency,
-              readOnly: true,
-            } :
-              {
-                inputComponent: NumberFormatCurrency,
-                readOnly: true,
+            />
+            <TextField
+              id="tax"
+              label={`Tax (${(state.taxPercent * 100).toFixed(2)}%)`}
+              margin="normal"
+              variant="outlined"
+              fullWidth
+              value={tax > 0 ? tax : ''}
+              onChange={handleChange('tax')}
+              InputProps={
+                state.tax > 0 ? {
+                  startAdornment: <InputAdornment position="start">$</InputAdornment>,
+                  inputComponent: NumberFormatCurrency,
+                } :
+                  {
+                    inputComponent: NumberFormatCurrency,
+                  }
               }
-          }
-        />
-        <Grid container justify='space-between'>
-          <Grid item xs={3} >
-            <Button aria-label="Round down" fullWidth variant='outlined' onClick={() => handleClick('down')}>Down</Button>
-          </Grid>
-          <Grid item xs={6} >
-            <Button aria-label="Round total" fullWidth >Round Total</Button>
-          </Grid>
-          <Grid item xs={3}>
-            <Button aria-label="Round up" fullWidth variant='outlined' onClick={() => handleClick('up')}>Up</Button>
-          </Grid>
-        </Grid>
-    </ThemeProvider>
-    </Container>
-    </Box>
+            />
+            <Slider
+              id='taxSlider'
+              valueLabelDisplay="auto"
+              valueLabelFormat={() => parseInt(state.taxPercent * 100) + '%'}
+              step={1}
+              marks
+              min={0}
+              max={30}
+              value={parseInt(state.taxPercent * 100)}
+              onChange={handleChange('taxPercent')}
+            />
+            <TextField
+              id="tip"
+              label={`Tip (${(state.tipPercent * 100).toFixed(2)}%)`}
+              margin="normal"
+              variant="outlined"
+              fullWidth
+              value={tip > 0 ? tip : ''}
+              onChange={handleChange('tip')}
+              InputProps={
+                state.tip > 0 ? {
+                  startAdornment: <InputAdornment position="start">$</InputAdornment>,
+                  inputComponent: NumberFormatCurrency,
+                } :
+                  {
+                    inputComponent: NumberFormatCurrency,
+                  }
+              }
+            />
+            <Slider
+              id='tipSlider'
+              valueLabelDisplay="auto"
+              valueLabelFormat={() => parseInt(state.tipPercent * 100) + '%'}
+              step={1}
+              marks
+              min={0}
+              max={30}
+              value={parseInt(state.tipPercent * 100)}
+              onChange={handleChange('tipPercent')}
+            />
+            <TextField
+              id="total"
+              label='Total'
+              margin="normal"
+              variant="outlined"
+              fullWidth
+              value={total > 0 ? total : ''}
+              onChange={handleChange('total')}
+              InputProps={
+                state.total > 0 ? {
+                  startAdornment: <InputAdornment position="start">$</InputAdornment>,
+                  inputComponent: NumberFormatCurrency,
+                  readOnly: true,
+                } :
+                  {
+                    inputComponent: NumberFormatCurrency,
+                    readOnly: true,
+                  }
+              }
+            />
+            <Grid container justify='space-between'>
+              <Grid item xs={3} >
+                <Button aria-label="Round down" fullWidth variant='outlined' onClick={() => handleClick('down')}>Down</Button>
+              </Grid>
+              <Grid item xs={6} >
+                <Button aria-label="Round total" fullWidth >Round Total</Button>
+              </Grid>
+              <Grid item xs={3}>
+                <Button aria-label="Round up" fullWidth variant='outlined' onClick={() => handleClick('up')}>Up</Button>
+              </Grid>
+            </Grid>
+          </ThemeProvider>
+        </Container>
+      </Container>
     </React.Fragment>
   );
 }
